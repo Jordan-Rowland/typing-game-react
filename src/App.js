36df236bf0c714246122858b79c9b1ef247c5896
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-02-06 20:59:27 -0800

    creates timer function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 function App() {
-    return (
-        <div>
-            <h1>How fast do you type?</h1>
-            <textarea />
-            <h4>Time reminaing: ???</h4>
-            <button>Start</button>
-            <h1>Word count: ???</h1>
-        </div>
-    )
+  const [wordsInput, setWordsInput] = useState("");
+  const [numWords, setnumWords] = useState();
+  const [timer, setTimer] = useState(3);
+
+  function handleChange(e) {
+    setWordsInput(e.target.value)
+    const wordSplitList = wordsInput.trim().split(" ")
+    const cleanWordList = wordSplitList.filter(word => word !== "")
+    setnumWords(cleanWordList.length)
+  }
+
+  useEffect(() => {
+    if (timer > 0) {
+      const timeoutId = setTimeout(() => {
+        setTimer(prevTimer => prevTimer - 1)
+      }, 1000)
+    }
+  }, [timer])
+
+  return (
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea onChange={handleChange} value={wordsInput}/>
+      <h4>Time reminaing: {timer}</h4>
+      <button>Start</button>
+      <h1>Word count: {numWords}</h1>
+    </div>
+  )
 }
 
 export default App
