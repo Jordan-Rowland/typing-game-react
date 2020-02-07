1da39644ab8fe67ffe8ada90ebc91ad30aaf064e
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-02-07 08:50:42 -0800

    completes mvp base game

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 function App() {
+const STARTING_TIME = 4
+
   const [wordsInput, setWordsInput] = useState("");
   const [numWords, setnumWords] = useState();
-  const [timer, setTimer] = useState(3);
+  const [timerRemaining, setTimerRemaining] = useState(STARTING_TIME);
+  const [gameRunning, setGameRunning] = useState(false);
+  const textInputRef = useRef(null);
 
   function handleChange(e) {
-    setWordsInput(e.target.value)
-    const wordSplitList = wordsInput.trim().split(" ")
-    const cleanWordList = wordSplitList.filter(word => word !== "")
-    setnumWords(cleanWordList.length)
+    if (gameRunning) {
+      setWordsInput(e.target.value);
+    }
   }
 
+  function startGame() {
+    setTimerRemaining(STARTING_TIME);
+    setGameRunning(true);
+    setWordsInput("");
+    setnumWords(0);
+    console.log(textInputRef)
+    textInputRef.current.disabled = false;
+    textInputRef.current.focus();
+  }
+
+  function endGame() {
+    setGameRunning(false);
+    const wordSplitList = wordsInput.trim().split(" ");
+    const cleanWordList = wordSplitList.filter(word => word !== "");
+    setnumWords(cleanWordList.length);
+  }
+
+
   useEffect(() => {
-    if (timer > 0) {
-      const timeoutId = setTimeout(() => {
-        setTimer(prevTimer => prevTimer - 1)
-      }, 1000)
+    if (timerRemaining > 0 && gameRunning) {
+      setTimeout(() => {
+        setTimerRemaining(prevTimer => prevTimer - 1)
+      }, 1000);
+    } else if (timerRemaining === 0) {
+      endGame();
     }
-  }, [timer])
+  }, [timerRemaining, gameRunning])
+
+  console.log(gameRunning);
 
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea onChange={handleChange} value={wordsInput}/>
-      <h4>Time reminaing: {timer}</h4>
-      <button>Start</button>
+      <textarea
+        ref={textInputRef}
+        disabled={!gameRunning}
+        onChange={handleChange}
+        value={wordsInput}
+      />
+      <h4>Time reminaing: {timerRemaining}</h4>
+      <button disabled={gameRunning} onClick={startGame}>Start</button>
       <h1>Word count: {numWords}</h1>
     </div>
   )
